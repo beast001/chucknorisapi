fdf4c391a54e7a68876196ec7d9067c75952e37a
beast001 <39857500+beast001@users.noreply.github.com>
2021-09-16 12:36:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect, useState} from 'react';
+import {Card, CardContent, CardActions, CssBaseline, Container, Typography} from '@material-ui/core';
+import axios from 'axios';
 function App() {
+const [jokes, setCategory] = useState([])
+const [state, setState] = useState({
+joke:''
+})
+
+useEffect (()=>{
+fetchData();
+},[]);
+
+const fetchData = async ()=>{
+const results = await axios.get('https://api.chucknorris.io/jokes/random?category=science');
+console.log(results);
+setState({
+...state,
+joke:results.data.value})
+};
+
+
+useEffect(() =>{
+fetch('https://api.chucknorris.io/jokes/categories')
+.then(response =>response.json())
+.then(response => {
+console.log(response)
+
+setCategory(response)
+})
+.catch(err=>console.log(err))
+},[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <CssBaseline/>
+    <container>
+     <Typography variant="h1" align="center">
+     Chuck Norris Jokes
+     </Typography>
+     <Typography align="center">
+     <select onclick='reload' name="cars" id="cars">
+                   <option value="animal">Animal</option>
+                   <option value="science">Science</option>
+                   <option value="career">Career</option>
+                   <option value="dev">Dev</option>
+
+                    <option value="celebrity">Celebrity</option>
+                    <option value="explicit">Explicit</option>
+                    <option value="fashion">Fashion</option>
+                     <option value="food">Food</option>
+                      <option value="history">History'</option>
+                       <option value="money">Money</option>
+                       <option value="movie">Movie</option>
+                       <option value="music">Music</option>
+                       <option value="political">Political</option>
+                       <option value="religion">Religion</option>
+                        <option value="sport">Sport</option>
+                        <option value="travel">Travel</option>
+
+                 </select>
+                 </Typography>
+    <card>
+    <cardContent>
+    <Typography>{state.joke}</Typography>
+    </cardContent>
+    </card>
+
+    </container>
     </div>
   );
 }
